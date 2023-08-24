22eebd67086a1310168beed2df8578d36ba64f54
DiegoCoura <diegogomes.10.9@gmail.com>
2023-08-24 00:54:26 -0300

    how to remove a single element from an array

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR"
+    let multiWords = "";
+    for(i = 0; i < num; i++){
+        multiWords += string;
+    }
+   return multiWords;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const wordArray = string.split("");
+    const wordArrayReverse = wordArray.reverse();
+    const wordReverse = wordArrayReverse.join("");
 
+    return wordReverse;
 };
 
 // Do not edit below this line
