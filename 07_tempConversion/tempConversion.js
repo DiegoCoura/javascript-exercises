96af164182bf3495125da8a4e7cb640af6e17446
DiegoCoura <diegogomes.10.9@gmail.com>
2023-08-24 18:23:09 -0300

    feat: exercise 07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(fahrenheit) {
+  const inCelsius = parseFloat(((fahrenheit - 32) / 1.8).toFixed(1));  
+  return  inCelsius;
+};  
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const inFahrenheit = parseFloat(((celsius * 1.8) + 32).toFixed(1));
+  return inFahrenheit;
 };
 
 // Do not edit below this line
