eab74f9ad7b591d7f94082bd240d2f54ff2f0e99
DiegoCoura <diegogomes.10.9@gmail.com>
2023-10-20 00:45:15 -0300

    ex 8 and 9 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let re = /[\W_]/g;
+    const processedString = string.toLowerCase().replace(re, "");
+    return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
